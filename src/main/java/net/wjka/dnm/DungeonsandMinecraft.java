ac8929d9515068c763e7cf043a987e044baa358c
Hannah-Ga <justin.wz@outlook.com>
2024-01-17 13:23:59 +0100

    created files

---
+++ src/main/java/net/wjka/dnm/DungeonsandMinecraft.java
@@ -0,0 +1,31 @@
+package net.wjka.dnm;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DungeonsandMinecraft implements ModInitializer {
+	public static final String MOD_ID = "dungeons_and_minecraft"; //important String. DON'T EDIT
+
+	public static final Item TestBlock = new Item(new FabricItemSettings());
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		Registry.register(Registries.ITEM, new Identifier("dungeons", "testblock"), TestBlock);// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
