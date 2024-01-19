ebd033c33c52ec98884976bb34c5da6ddaa0f71e
Hannah-Ga <justin.wz@outlook.com>
2024-01-19 16:17:16 +0100

    commit changes1

--- src/main/java/net/wjka/dnm/DungeonsandMinecraft.java
+++ src/main/java/net/wjka/dnm/DungeonsandMinecraft.java
@@ -3,6 +3,9 @@ package net.wjka.dnm;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
@@ -13,7 +16,9 @@ import org.slf4j.LoggerFactory;
 public class DungeonsandMinecraft implements ModInitializer {
 	public static final String MOD_ID = "dungeons_and_minecraft"; //important String. DON'T EDIT
 
-	public static final Item TestBlock = new Item(new FabricItemSettings());
+	public static final Block TestBlock = new Block(FabricBlockSettings.create().strength(1.5f));
+
+	//public static final Item TestItem = new Item(new FabricItemSettings());
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -25,7 +30,10 @@ public class DungeonsandMinecraft implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Registry.register(Registries.ITEM, new Identifier("dungeons", "testblock"), TestBlock); //created a TestBlock. Works!
+		Registry.register(Registries.ITEM, new Identifier("dungeons", "testblock"), new BlockItem(TestBlock, new FabricItemSettings()));
+
+		Registry.register(Registries.BLOCK, new Identifier("dungeons", "testblock"), TestBlock); //created a TestBlock. Works!
+
 
 
 		LOGGER.info("Hello Fabric world!");
