2d7d0e842395546490f2389b1ac85c6b6eb6f5dc
Hannah-Ga <justin.wz@outlook.com>
2024-01-31 22:49:46 +0100

    added new method for initializing items... added example item with image and name

--- src/main/java/net/wjka/dnm/DungeonsandMinecraft.java
+++ src/main/java/net/wjka/dnm/DungeonsandMinecraft.java
@@ -2,14 +2,6 @@ package net.wjka.dnm;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +9,8 @@ public class DungeonsandMinecraft implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final String MOD_ID = "dungeons_and_minecraft"; //important String. DON'T EDIT
+	public static final String MOD_ID = "dungeons_and_minecraft"; //MOD ID String. DON'T EDIT
+
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 
@@ -27,7 +20,8 @@ public class DungeonsandMinecraft implements ModInitializer {
 		   However, some things (like resources) may still be uninitialized.
 		   Proceed with mild caution.*/
 
-		RegisterItems.InitRegister(); //Registring of all Items will be done via this method!
+		RegisterItems.InitRegisterItems(); //Registring of all Items will be done via this method!
+		RegisterBlocks.InitRegisterBlocks(); //Registring of all Blocks
 
 
 
--- src/main/java/net/wjka/dnm/RegisterItems.java
+++ src/main/java/net/wjka/dnm/RegisterItems.java
@@ -11,20 +11,23 @@ import net.minecraft.item.Item;
 import net.minecraft.registry.Registry; //use this registry not java.rmi.registry!
 
 public class RegisterItems {
+    //declarations of Items :3
     public static final Item TESTITEM = registerItems("testitem", new Item(new FabricItemSettings())); //init item
 
     private static void addItemsToInv(FabricItemGroupEntries entries){ //is used to add them to the player (creative) inventory
         entries.add(TESTITEM);
-    }
+    } //as said... add items to creative inventory :3
 
-    private static Item registerItems(String name, Item item){
+    private static Item registerItems(String name, Item item){ //handles registering our items, so we dont need to write this manually for every item
         return Registry.register(Registries.ITEM, new Identifier(DungeonsandMinecraft.MOD_ID, name), item);
     }
-    public static void InitRegister(){
-        DungeonsandMinecraft.LOGGER.info("Registring Started" + DungeonsandMinecraft.MOD_ID); //Logging for the console...
+    public static void InitRegisterItems(){
+        DungeonsandMinecraft.LOGGER.info("Registring of Items Started" + DungeonsandMinecraft.MOD_ID); //Logging for the console...
+
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(RegisterItems::addItemsToInv);//should be last piece of code to be run in order to add all items
         //were initialised before!
         }
+        //add item to itemgroup ingredients
     }
 
