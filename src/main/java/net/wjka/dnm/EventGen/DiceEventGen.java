757b580d934a2af8b8ab32d0675a9ebce8524e46
Hannah-Ga <justin.wz@outlook.com>
2024-02-04 18:13:42 +0100

    committttt

---
+++ src/main/java/net/wjka/dnm/EventGen/DiceEventGen.java
@@ -0,0 +1,31 @@
+package net.wjka.dnm.EventGen;
+
+public class DiceEventGen {
+
+    int DiceNum;
+    String DiceType;
+
+    public void DecideEvent(int pDiceNum, String pDiceType){
+
+    this.DiceNum = pDiceNum;
+    this.DiceType = pDiceType;
+
+
+
+    }
+
+
+    //all events that might happen will be listed as methods in here. methods will be implemented in many other classes!
+    private void NegaticeDiceEvent(){
+
+    }
+
+    private void NeutralDiceEvent(){
+
+    }
+
+    private void PositiveDiceEvent(){
+
+    }
+
+}
