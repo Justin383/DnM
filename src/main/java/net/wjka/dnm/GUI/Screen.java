f9befa7275f6f655ce4a0bebb4db3eee8e450e0a
Aleksandra27 <aleksandra2004k2@gmail.com>
2024-02-10 18:24:15 +0100

    gui erstellt

---
+++ src/main/java/net/wjka/dnm/GUI/Screen.java
@@ -0,0 +1,10 @@
+package net.wjka.dnm.GUI;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class Screen extends CottonClientScreen{
+    public Screen(GuiDescription description) {
+        super(description);
+    }
+}
--- src/main/java/net/wjka/dnm/item/Dice/NegativeDice.java
+++ src/main/java/net/wjka/dnm/item/Dice/NegativeDice.java
@@ -1,5 +1,6 @@
 package net.wjka.dnm.item.Dice;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -7,6 +8,9 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import net.wjka.dnm.GUI.Gui;
+import net.wjka.dnm.GUI.Screen;
+
 
 public class NegativeDice extends Item {
 
@@ -23,6 +27,9 @@ public class NegativeDice extends Item {
         if (!world.isClient) { //need to check that this is only called serverside to ensure that it wont be called twice in SP worlds
             dR.RollDice("negative"); //calls the function to roll a dice
         }
+
+        //opens GUI upon right-click:
+        MinecraftClient.getInstance().setScreen(new Screen(new Gui()));
 
         return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
     }
