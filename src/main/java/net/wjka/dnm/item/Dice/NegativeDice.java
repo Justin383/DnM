5c427463a494c5873d456f7268a3f6f814566687
Hannah-Ga <justin.wz@outlook.com>
2024-03-11 10:34:29 +0100

    gwui

--- src/main/java/net/wjka/dnm/item/Dice/NegativeDice.java
+++ src/main/java/net/wjka/dnm/item/Dice/NegativeDice.java
@@ -1,16 +1,21 @@
 package net.wjka.dnm.item.Dice;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import net.wjka.dnm.GUI.CustomPopupScreen;
 
 
 public class NegativeDice extends Item {
+
 
 
     DiceRoll dR = new DiceRoll();
@@ -31,6 +36,10 @@ public class NegativeDice extends Item {
             ServerWorld serverWorld = (ServerWorld)world;
             // Pass serverWorld to the RollDice method
             dR.RollDice("dice_negative", serverWorld, user);
+        }
+        if (world.isClient) {
+            CustomPopupScreen cps = new CustomPopupScreen(Text.literal("hi mum"));
+            MinecraftClient.getInstance().setScreen(cps); // This line will display your custom screen
         }
         return new TypedActionResult<>(ActionResult.SUCCESS, itemStack);
     }
